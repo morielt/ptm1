e63fc6178fc2f7e0948fe4fd78f42045c5904ef9
morielt <165683689+morielt@users.noreply.github.com>
2024-12-23 18:01:26 +0200

    Create constructors.
    Add three constructors.

--- Message.java
+++ Message.java
@@ -8,5 +8,27 @@ public class Message {
     public final double asDouble;
     public final Date date;
 
+    public Message(byte[] data) {
+        this.data = data;
+        this.asText = data.toString();
+        this.asDouble = tryParse(this.asText);
+        this.date = new Date();
+    }
 
+    public Message(String asText) {
+        this(asText.getBytes());
+    }
+
+    public Message(double asDouble) {
+        this(Double.toString(asDouble));
+    }
+
+    private double tryParse(String data){
+        try {
+            return Double.parseDouble(data);
+        } catch (NumberFormatException exception){
+            System.out.println(exception.getMessage());
+            return Double.NaN;
+        }
+    }
 }
